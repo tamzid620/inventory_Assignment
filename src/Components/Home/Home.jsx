adfa9cd435672e39cfc25a75c413a5100829dfbe
tamzid620 <tamzid620@gmail.com>
2024-05-28 12:17:49 +0600

    table and tab fragment added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import { useColorMode, Button, Box, Text } from "@chakra-ui/react";
+import {
+  useColorMode,
+  Button,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Flex
+} from "@chakra-ui/react";
 
 const Home = () => {
   const navigation = useNavigate();
@@ -9,7 +27,7 @@ const Home = () => {
   const token = localStorage?.token;
   const user = localStorage?.user;
   if (token && user) {
-    window.location.reload();
+    ("");
   } else {
     Swal.fire({
       position: "center",
@@ -22,20 +40,98 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1 className="text-3xl text-center underline uppercase ">
-        <Box textAlign="center" py={10} px={6}>
-          <Text
-            fontSize="3xl"
-            className="text-3xl text-center underline uppercase"
-          >
-            Welcome to Home
-          </Text>
-          <Button onClick={toggleColorMode} mt={4}>
-            Toggle {colorMode === "light" ? "Dark" : "Light"}
-          </Button>
-        </Box>
-      </h1>
+    <div className="mx-10">
+      <h1 className="text-3xl text-center  uppercase ">
+        {/* dark and light button  */}
+        <Button onClick={toggleColorMode} mt={4}>
+          Toggle {colorMode === "light" ? "Dark" : "Light"}
+        </Button>
+        </h1>
+        {/* Tab section  */}
+        <Tabs variant="unstyled">
+        <Flex mb={10} justifyContent="space-between">
+          <TabList>
+            <Tab _selected={{ color: "white", bg: "blue.500" }}>
+              Active Sale Orders
+            </Tab>
+            <Tab _selected={{ color: "white", bg: "green.400" }}>
+              Completed Sale Orders
+            </Tab>
+          </TabList>
+          <Button colorScheme='green'>+ Sale Order</Button>
+          </Flex>
+          <TabPanels>
+            {/* TabPanel 1  */}
+            <TabPanel>
+              <TableContainer>
+                <Table variant="simple">
+                  <TableCaption>
+                    Imperial to metric conversion factors
+                  </TableCaption>
+                  <Thead>
+                    <Tr>
+                      <Th>Index</Th>
+                      <Th>Id</Th>
+                      <Th>Customer Name</Th>
+                      <Th isNumeric>Price</Th>
+                      <Th isNumeric>Last Modified </Th>
+                      <Th isNumeric>Edit /view</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td></Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </TabPanel>
+            {/* TabPanel -2  */}
+            <TabPanel>
+              <TableContainer>
+                <Table variant="simple">
+                  <TableCaption>
+                    Imperial to metric conversion factors
+                  </TableCaption>
+                  <Thead>
+                    <Tr>
+                      <Th>To convert</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>feet</Td>
+                      <Td>centimetres (cm)</Td>
+                      <Td isNumeric>30.48</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>yards</Td>
+                      <Td>metres (m)</Td>
+                      <Td isNumeric>0.91444</Td>
+                    </Tr>
+                  </Tbody>
+                  <Tfoot>
+                    <Tr>
+                      <Th>To convert</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                    </Tr>
+                  </Tfoot>
+                </Table>
+              </TableContainer>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+
     </div>
   );
 };
